6db2dea4b3724328bb7a70e58a50f17de7c8b926
CelesteSMThomas <celestesmthomas@gmail.com>
2021-03-16 13:35:28 -0700

    commit tree f00e06cb4395e7f04a2f8aff94a596fabc188706

--- main.js
+++ main.js
@@ -4,25 +4,30 @@ const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
 const likeHearts = document.querySelectorAll(`.like-glyph`);
+const errorDiv = document.getElementById(`modal`);
+errorDiv.classList.add("hidden");
 
 for (let i = 0; i < likeHearts.length; i++)
   likeHearts[i].addEventListener("click", function () {
     mimicServerCall()
-      .then(status => function())
-      .then((res) => res.json())
+      .then((status) => {
+        if (likeHearts[i].className === "like-glyph") {
+          likeHearts[i].classList.remove("like-glyph");
+          likeHearts[i].classList.add("activated-heart");
+          likeHearts[i].innerText = `${FULL_HEART}`;
+          errorDiv.classList.add(`hidden`);
+        } else {
+          likeHearts[i].classList.remove("activated-heart");
+          likeHearts[i].classList.add("like-glyph");
+          likeHearts[i].innerText = `${EMPTY_HEART}`;
+          errorDiv.classList.add(`hidden`);
+        }
+      })
       .catch((error) => {
+        alert(`${error}`);
+        errorDiv.classList.remove(`hidden`);
         throw error;
       });
-    console.log("The Heart has been pressed");
-    if (likeHearts[i].className === "like-glyph") {
-      likeHearts[i].classList.remove("like-glyph");
-      likeHearts[i].classList.add("activated-heart");
-      likeHearts[i].innerText = `${FULL_HEART}`;
-    } else {
-      likeHearts[i].classList.remove("activated-heart");
-      likeHearts[i].classList.add("like-glyph");
-      likeHearts[i].innerText = `${EMPTY_HEART}`;
-    }
   });
 
 //------------------------------------------------------------------------------
